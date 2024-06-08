a2d9de03d33c2537356531f7b02b822bb0e2fb91
Bradley Staples <bradleystaples@fastmail.com>
2024-06-07 19:54:06 -0700

    Update icons in use

--- scripts/dnd5e-status-icons-2.js
+++ scripts/dnd5e-status-icons-2.js
@@ -6,28 +6,18 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/advantage.png"
     },
     {
-        "id": "ancenstral-protectors",
-        "label": "Ancenstral Protectors",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/ancenstral-protectors.png"
-    },
-    {
-        "id": "armor-of-agathys",
-        "label": "Armor of Agathys",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/armor-of-agathys.png"
-    },
-    {
         "id": "baned",
         "label": "Baned",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/baned.png"
     },
     {
-        "id": "bears-endurance",
-        "label": "Bear's Endurance",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/bears-endurance.png"
+        "id": "behind-cover",
+        "label": "Behind Cover",
+        "icon": "modules/dnd5e-status-icons-2/img/conditions/behind-cover.png"
     },
     {
-        "id": "bleeding-out",
-        "label": "Bleeding Out",
+        "id": "bleeding",
+        "label": "Bleeding",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/bleeding-out.png"
     },
     {
@@ -41,29 +31,9 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/blinded.png"
     },
     {
-        "id": "blink",
-        "label": "Blink",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/blink.png"
-    },
-    {
-        "id": "blur",
-        "label": "Blur",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/blur.png"
-    },
-    {
-        "id": "bulls-strength",
-        "label": "Bull's Strength",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/bulls-strength.png"
-    },
-    {
-        "id": "cats-grace",
-        "label": "Cat's Grace",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/cats-grace.png"
-    },
-    {
-        "id": "cause-of-fear",
-        "label": "Cause of Fear",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/cause-of-fear.png"
+        "id": "burning",
+        "label": "Burning",
+        "icon": "modules/dnd5e-status-icons-2/img/conditions/on-fire.png"
     },
     {
         "id": "charmed",
@@ -71,19 +41,19 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/charmed.png"
     },
     {
-        "id": "compelled-duel",
-        "label": "Compelled Duel",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/compelled-duel.png"
-    },
-    {
-        "id": "concentration",
-        "label": "Concentration",
+        "id": "concentrating",
+        "label": "Concentrating",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/concentration.png"
     },
     {
         "id": "confused",
         "label": "Confused",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/confused.png"
+    },
+    {
+        "id": "cursed",
+        "label": "Cursed",
+        "icon": "systems/dnd5e/icons/svg/statuses/cursed.svg"
     },
     {
         "id": "deafened",
@@ -96,13 +66,8 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/disadvantage.png"
     },
     {
-        "id": "divine-favor",
-        "label": "Divine Favor",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/divine-favor.png"
-    },
-    {
-        "id": "dodge-action",
-        "label": "Dodge Action",
+        "id": "dodging",
+        "label": "Dodging",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/dodge-action.png"
     },
     {
@@ -111,14 +76,19 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "icons/svg/skull.svg"
     },
     {
-        "id": "eagles-splendor",
-        "label": "Eagle's Splendor",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/eagles-splendor.png"
+        "id": "diseased",
+        "label": "Diseased",
+        "icon": "systems/dnd5e/icons/svg/statuses/diseased.svg"
     },
     {
         "id": "entangled",
         "label": "Entangled",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/entangled.png"
+    },
+    {
+        "id": "ethereal",
+        "label": "Ethereal",
+        "icon": "systems/dnd5e/icons/svg/statuses/ethereal.svg"
     },
     {
         "id": "exhausted",
@@ -129,11 +99,6 @@ const dnd5eStatusIcons2 = (() => {
         "id": "flying",
         "label": "Flying",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/flying.png"
-    },
-    {
-        "id": "foxs-cunning",
-        "label": "Fox's Cunning",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/foxs-cunning.png"
     },
     {
         "id": "frightened",
@@ -151,39 +116,19 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/hasted.png"
     },
     {
-        "id": "hex",
-        "label": "Hex",
+        "id": "hiding",
+        "label": "Hiding",
+        "icon": "systems/dnd5e/icons/svg/statuses/hiding.svg"
+    },
+    {
+        "id": "hexed",
+        "label": "Hexed",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/hex.png"
-    },
-    {
-        "id": "hexblades-curse",
-        "label": "Hexblade's Curse",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/hexblades-curse.png"
-    },
-    {
-        "id": "highlighted",
-        "label": "Highlighted",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/highlighted.png"
-    },
-    {
-        "id": "holding-action",
-        "label": "Holding Action",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/holding-action.png"
     },
     {
         "id": "incapacitated",
         "label": "Incapacitated",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/incapacitated.png"
-    },
-    {
-        "id": "insightful-fighting",
-        "label": "Insightful Fighting",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/insightful-fighting.png"
-    },
-    {
-        "id": "inspired",
-        "label": "Inspired",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/inspired.png"
     },
     {
         "id": "invisible",
@@ -196,24 +141,9 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/mage-armor.png"
     },
     {
-        "id": "mark",
-        "label": "Mark",
+        "id": "marked",
+        "label": "Marked",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/mark.png"
-    },
-    {
-        "id": "mirror-image",
-        "label": "Mirror Image",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/mirror-image.png"
-    },
-    {
-        "id": "on-fire",
-        "label": "On Fire",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/on-fire.png"
-    },
-    {
-        "id": "owls-wisdom",
-        "label": "Owl's Wisdom",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/owls-wisdom.png"
     },
     {
         "id": "paralyzed",
@@ -231,11 +161,6 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/poisoned.png"
     },
     {
-        "id": "possessed",
-        "label": "Possessed",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/possessed.png"
-    },
-    {
         "id": "prone",
         "label": "Prone",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/prone.png"
@@ -246,19 +171,9 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/raging.png"
     },
     {
-        "id": "reaction-used",
-        "label": "Reaction Used",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/reaction-used.png"
-    },
-    {
         "id": "restrained",
         "label": "Restrained",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/restrained.png"
-    },
-    {
-        "id": "sanctuary",
-        "label": "Sanctuary",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/sanctuary.png"
     },
     {
         "id": "shell-defense",
@@ -266,19 +181,19 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/shell-defense.png"
     },
     {
-        "id": "shield-of-faith",
-        "label": "Shield of Faith",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/shield-of-faith.png"
+        "id": "silenced",
+        "label": "Silenced",
+        "icon": "systems/dnd5e/icons/svg/statuses/silenced.svg"
     },
     {
-        "id": "shifted",
-        "label": "Shifted",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/shifted.png"
+        "id": "sleeping",
+        "label": "Sleeping",
+        "icon": "systems/dnd5e/icons/svg/statuses/sleeping.svg"
     },
     {
-        "id": "slayers-prey",
-        "label": "Slayer's Prey",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/slayers-prey.png"
+        "id": "slowed",
+        "label": "Slowed",
+        "icon": "modules/dnd5e-status-icons-2/img/conditions/slowed.png"
     },
     {
         "id": "spirit-guardians",
@@ -296,14 +211,14 @@ const dnd5eStatusIcons2 = (() => {
         "icon": "modules/dnd5e-status-icons-2/img/conditions/stunned.png"
     },
     {
-        "id": "summoning",
-        "label": "Summoning",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/summoning.png"
+        "id": "surprised",
+        "label": "Surprised",
+        "icon": "systems/dnd5e/icons/svg/statuses/surprised.svg"
     },
     {
-        "id": "symbiotic-entity",
-        "label": "Symbiotic Entity",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/symbiotic-entity.png"
+        "id": "transformed",
+        "label": "Transformed",
+        "icon": "systems/dnd5e/icons/svg/statuses/transformed.svg"
     },
     {
         "id": "truesight",
@@ -314,19 +229,8 @@ const dnd5eStatusIcons2 = (() => {
         "id": "unconcious",
         "label": "Unconcious",
         "icon": "modules/dnd5e-status-icons-2/img/conditions/unconcious.png"
-    },
-    {
-        "id": "warding-bond",
-        "label": "Warding Bond",
-        "icon": "modules/dnd5e-status-icons-2/img/conditions/warding-bond.png"
     }];
-    // removed wounded because it was the only red icon
-    // plus it made for a better 8x8 grid of icon
-    // {
-    //     "id": "wounded",
-    //     "label": "Wounded",
-    //     "icon": "modules/dnd5e-status-icons-2/img/conditions/wounded.svg"
-    // }
+
   };
 
   Hooks.once("ready", function () {
